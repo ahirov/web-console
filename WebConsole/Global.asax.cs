529b47ac26f56c34a0d5edabcec1fb2d6ee74fe7
Anton Hirov <hirov.anton@yahoo.com>
2018-11-13 17:08:46 +0200

    Added initial sln project

---
+++ WebConsole/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebConsole
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
