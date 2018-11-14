02afed09f08132433550cb928fdd6806eedc0af4
Anton Hirov <hirov.anton@yahoo.com>
2018-11-14 19:12:10 +0200

    Added Core project

---
+++ WebConsole/Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebConsole.Core
+{
+    public class Class1
+    {
+    }
+}
