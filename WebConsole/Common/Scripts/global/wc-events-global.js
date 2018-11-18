f63efbe0cae9a5c1a57645aff2c852f8a64e7d17
Anton Hirov <hirov.anton@yahoo.com>
2018-11-18 23:57:55 +0200

    Completed WebConsole header

---
+++ WebConsole/Common/Scripts/global/wc-events-global.js
@@ -0,0 +1,9 @@
+﻿/* WebConsole (https://github.com/hirov-anton/web-console)
+See LICENSE file in the solution root for full license information
+Copyright (c) 2018 Anton Hirov */
+
+$(document).ready(function() {
+    $(function() {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+});
